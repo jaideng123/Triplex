e29f5e8c86ee40b966233b5ee909ee65d27a0e2d
jaideng123 <jaideng123@yahoo.com>
2015-08-12 14:03:08 -0500

    Keeps track of pieces played

--- triplex.js
+++ triplex.js
@@ -4,6 +4,20 @@ var update_board = function(board){
 			$('#'+levels[i] + (j+1)).html(symbols[board[i][j]]);
 		};
 	};
+	var leftVals = $('form#left span.remaining')
+	for (var i = leftVals.length - 1; i >= 0; i--) {
+		if(i == leftVals.length - 1)
+			leftVals.eq(i).html(pieces[6]);
+		else
+			leftVals.eq(i).html(pieces[i]);
+	};
+	var rightVals = $('form#right span.remaining')
+	for (var i = rightVals.length - 1; i >= 0; i--) {
+		if(i == rightVals.length - 1)
+			rightVals.eq(i).html(pieces[7]);
+		else
+			rightVals.eq(i).html(pieces[i+3]);
+	};
 };
 var findCurrentSymbol = function(turn){
 	if(turn == 0)
@@ -35,6 +49,7 @@ var levels = ['T','M','B'];
 var symbols = ['X','<i class="fa fa-caret-square-o-up fa-2x"></i>','<i class="fa fa-square-o fa-2x"></i>','<i class="fa fa-circle-o fa-2x"></i>',
 '<i class="fa fa-caret-up fa-2x"></i>','<i class="fa fa-square fa-2x"></i>','<i class="fa fa-circle fa-2x"></i>','<i class="fa fa-star-o fa-2x"></i>','<i class="fa fa-star fa-2x"></i>'];
 var symbolNames = ['','triangle','square','circle','triangle-b','square-b','circle-b','wild','wild-b'];
+var pieces = [3,3,3,3,3,3,1,1]
 var turn = 0;
 $(document).ready(function(){
 	$('.right-player form input').attr('disabled',true);
@@ -42,6 +57,7 @@ $(document).ready(function(){
 	$('.grid-btn').click(function() {
   		console.log("Clicked: " + this.id);
   		board[levels.indexOf(this.id[0])][this.id[1]-1] = findCurrentSymbol(turn);
+  		pieces[findCurrentSymbol(turn)-1]--;
   		update_board(board);
   		nextTurn();
 	});
